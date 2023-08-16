b5d6bd18d59c384fc2ce4cd6052e0234b2635958
0936311scogg <BPScoggins1@my.scccd.edu>
2023-08-16 11:01:57 -0700

    Added main functionality

--- largestOfThree.cpp
+++ largestOfThree.cpp
@@ -20,24 +20,42 @@
 // https://www.w3schools.com/cpp/cpp_strings.asp
 
 #include <iostream>
-using namespace std;
+#include <string>
 
 int main(){
-  cout << "\n\n Welcome to the largest of Three Program\n\n";
 
-  // Get the user's name
-  string userName = "";
-  cout << "\n Please enter your name: ";
-  cin >> userName;
+  int num1, num2, num3;
+  int largest;
+  std::string name;
 
-  //
+  std::cout << "\nWelcome to the Largest of Three Program.\n\n";
 
+  // INPUT
+  std::cout << "Please input your name: ";
+  std::getline(std::cin, name);
 
+  std::cout << "Please input the integers you would like to compare.\nFirst: ";
+  std::cin >> num1;
+  std::cout << "Second: ";
+  std::cin >> num2;
+  std::cout << "Third: ";
+  std::cin >> num3;
 
+  // PROCESSING
+  if (num1 > num2) {
+    largest = num1;
+  } else {
+    largest = num2;
+  }
+  if (num3 > largest) {
+    largest = num3;
+  }
 
+  // OUTPUT
+  std::cout << "\nThank you " << name << ".\n";
+  std::cout << "Your largest integer was " << largest << "." << std::endl;
 
   return 0;
-
 }
 
 
