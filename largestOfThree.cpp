9ab1d43d6b0a2eda67250b59060cabce026d5f93
0936311scogg <BPScoggins1@my.scccd.edu>
2023-08-16 11:03:34 -0700

    Updated heading

--- largestOfThree.cpp
+++ largestOfThree.cpp
@@ -1,23 +1,11 @@
 // largestOfThree.cpp
 //
-// Programmer's name: Dennis Mohle
+// Programmer's name: Benjamin Scoggins
 // Date: Aug 16, 2023
 //
-// Description of program:
-//
-// TODO: Describe your program in terms of input, processing, and output
-//
-// This program will input...
-//
-//
-// References:
-//
-// Getting started:
-// https://www.w3schools.com/cpp/cpp_getstarted.asp
-// User Input:
-// https://www.w3schools.com/cpp/cpp_user_input.asp
-// Strings:
-// https://www.w3schools.com/cpp/cpp_strings.asp
+// Input name and 3 integers.
+// Returns name and the largest integer
+
 
 #include <iostream>
 #include <string>
